c9cdbb60b66e6496587668fa448b0477ff188ff9
gnedeva <gnedeva@synchr.com>
2020-11-22 19:20:01 +0200

    initial version of security lab

---
+++ security/src/main/java/softuni/spring/security/web/HomeController.java
@@ -0,0 +1,22 @@
+package softuni.spring.security.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+}
